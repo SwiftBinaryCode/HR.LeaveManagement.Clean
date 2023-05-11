7b9b8d3a76bdb85653d5e55ce78cfab6aee3f15d
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2023-05-11 17:00:31 +0200

    Add project files.

---
+++ HR.LeaveManagement.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, int id) : base($"{name} ({id}) was not found")
+        {
+
+        }
+    }
+}
