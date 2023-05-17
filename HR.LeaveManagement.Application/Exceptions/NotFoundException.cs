d50df7775f8ea26a4bf2a3c3a76be7e93a5c1514
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2023-05-17 10:05:42 +0200

    editing ran into issues

--- HR.LeaveManagement.Application/Exceptions/NotFoundException.cs
+++ HR.LeaveManagement.Application/Exceptions/NotFoundException.cs
@@ -8,7 +8,7 @@ namespace HR.LeaveManagement.Application.Exceptions
 {
     public class NotFoundException : Exception
     {
-        public NotFoundException(string name, int id) : base($"{name} ({id}) was not found")
+        public NotFoundException(string name, object key) : base($"{name} ({key}) was not found")
         {
 
         }
